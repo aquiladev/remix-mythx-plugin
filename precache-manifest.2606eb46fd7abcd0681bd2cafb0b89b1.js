c998fb5d1e3ac3e9a900925c4b54e7750a4741a9
aquiladev <aquila.ua@gmail.com>
2019-05-20 13:26:39 +0000

    Automated deployment to GitHub Pages: 5e0af9bf34ad4f12fef9c417b50b29afa5fdd39d

---
+++ precache-manifest.2606eb46fd7abcd0681bd2cafb0b89b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e85e1e612e983b23db017f8dd834bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e05896d0de0f6ed9262",
+    "url": "/static/css/2.46738954.chunk.css"
+  },
+  {
+    "revision": "c60d9c5fae7b86cf5579",
+    "url": "/static/css/main.4699c619.chunk.css"
+  },
+  {
+    "revision": "1e05896d0de0f6ed9262",
+    "url": "/static/js/2.c2e82540.chunk.js"
+  },
+  {
+    "revision": "c60d9c5fae7b86cf5579",
+    "url": "/static/js/main.44a57efb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
