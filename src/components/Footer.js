1fc5f1c35330135d4b6b040486bdb9154dfc7f93
Sergii Bomko <aquila.ua@gmail.com>
2019-08-16 14:51:05 +0200

    fixes reference to release

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import packageJson from './../../package.json';
 function Footer({ isPlugin }) {
   return (
     <div className={`position-absolute text-center w-100 pb-1${isPlugin ? '' : ' text-white'}`} style={{ fontSize: 12, bottom: 0 }}>
-      <ExternalLink id='version' href={`https://github.com/aquiladev/remix-mythx-plugin/releases/tag/v${packageJson.version}`} target='_block' rel='noopener noreferrer'>
-        v{packageJson.version}
+      <ExternalLink id='version' href={`https://github.com/aquiladev/remix-mythx-plugin/releases/tag/${packageJson.version}`} target='_block' rel='noopener noreferrer'>
+        {packageJson.version}
       </ExternalLink> | <ExternalLink id='support' href='https://discord.gg/VhdtjCh' target='_blank' rel='noopener noreferrer'>Get support</ExternalLink>
     </div>
   );
