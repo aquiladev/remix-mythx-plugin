8fdede38f7c6f3bd0b62b177a5a9029154c1df27
Sergii Bomko <aquila.ua@gmail.com>
2019-12-21 04:47:10 +0200

    updates log messages

--- src/components/Log.js
+++ src/components/Log.js
@@ -14,8 +14,13 @@ function Log({ list = [] }) {
             return (
               <div key={i} className='col-md-6 offset-md-3 pt-1 pb-1'>
                 <span className='pr-2 text-secondary'>[{moment(x.timestamp).format('L LTS')}]</span>
-                {x.mode === 'full' ? <FontAwesomeIcon icon={faClock} className='mr-2' /> : null}
-                Your <b>{x.mode}</b> analysis has been submitted! Please see your results at
+                {x.mode === 'full' ?
+                  <>
+                    <FontAwesomeIcon icon={faClock} className='mr-2' />
+                    Request for <b>{x.mode}</b> analysis has been submitted. See your results shortly at
+                  </> :
+                  <>Your <b>{x.mode}</b> analysis is completed. See your results at</>
+                }
                 <a href={link} className='pl-1' target='_blank' rel='noopener noreferrer'>{x.uuid}</a>
               </div>
             );
